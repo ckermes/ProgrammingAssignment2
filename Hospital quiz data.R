fd6a901de13cc209d96f0ab15ff2e943e4f9208c
ckermes <ckermes2@gmail.com>
2020-04-16 12:24:07 -0400

    fixed load data, name of headers in temp file

--- Hospital quiz data.R
+++ Hospital quiz data.R
@@ -1,2 +1,3 @@
-hospitaldata <- read.csv('hospital-data.csv')
-outcomes <- read.csv('outcome-of-care-measures.csv')
+hospitaldata <- read.csv('hospital-data.csv', na.strings = "NA", stringsAsFactors = FALSE)
+outcomes <- read.csv('outcome-of-care-measures.csv', na.strings = "NA", stringsAsFactors = FALSE)
+names(hospitaldata) <- c('hospital', 'state', 'outcome')
